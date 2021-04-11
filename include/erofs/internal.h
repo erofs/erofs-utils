1f074e6bd77574e742342ce2772cbeeebd7a87b6
Gao Xiang <xiang@kernel.org>
2021-04-11 11:48:40 +0800

    erofs-utils: add big physical cluster definition
    This keeps in sync with the kernel definition.
    
    Link: https://lore.kernel.org/r/20210411034844.12673-5-xiang@kernel.org
    Signed-off-by: Gao Xiang <xiang@kernel.org>

--- include/erofs/internal.h
+++ include/erofs/internal.h
@@ -108,6 +108,7 @@ static inline void erofs_sb_clear_##name(void) \
 
 EROFS_FEATURE_FUNCS(lz4_0padding, incompat, INCOMPAT_LZ4_0PADDING)
 EROFS_FEATURE_FUNCS(compr_cfgs, incompat, INCOMPAT_COMPR_CFGS)
+EROFS_FEATURE_FUNCS(big_pcluster, incompat, INCOMPAT_BIG_PCLUSTER)
 EROFS_FEATURE_FUNCS(sb_chksum, compat, COMPAT_SB_CHKSUM)
 
 #define EROFS_I_EA_INITED	(1 << 0)
